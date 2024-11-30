a3f854296cd55ae0fc20bcea9dc91e36d3cb809c
blueonline07 <ldkhang1201@gmail.com>
2024-11-30 22:15:31 +0700

    orders and create order example

--- server.js
+++ server.js
@@ -3,12 +3,9 @@ import express from 'express';
 const app = express();
 const port = 3000;
 
-// Middleware
-app.use(express.static('views'));
 
-// app.get('/', (req, res) => {
-//   res.sendFile('index.html');
-// });
+app.use(express.static('public'));
+
 
 app.listen(port, () => {
   console.log(`Server running on http://localhost:${port}`);
