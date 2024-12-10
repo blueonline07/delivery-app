2a28fc4f503d0c5459f36a3fee2599c03bd5ac67
blueonline07 <ldkhang1201@gmail.com>
2024-12-11 02:48:51 +0700

    sql ok

--- sql/schema.sql
+++ sql/schema.sql
@@ -285,62 +285,6 @@ BEGIN
     END
 END;
 
-GO
-CREATE OR ALTER TRIGGER trg_CheckGiaoDich
-ON GiaoDich
-AFTER INSERT, UPDATE
-AS
-BEGIN
-    IF TRIGGER_NESTLEVEL() > 1
-        RETURN;
-
-    IF EXISTS (SELECT 1 FROM inserted)
-	BEGIN
-		IF EXISTS(
-			SELECT 1
-			FROM HoaDon H
-			JOIN inserted I ON H.maHoaDon = I.hoaDon
-			WHERE H.tinhTrang = N'Đã thanh toán' or H.tinhTrang = N'Đã hủy'
-		)
-		BEGIN
-			PRINT N'Bạn không thể giao dịch nữa.';
-			ROLLBACK TRANSACTION;
-		END
-		
-		IF EXISTS(
-			SELECT 1
-			FROM inserted I
-			WHERE I.tinhTrang = N'Thành công'
-		)
-		BEGIN
-			PRINT N'Bạn đã thanh toán thành công';
-			UPDATE HD
-			SET HD.tinhTrang = N'Đã Thanh Toán'
-			FROM HoaDon HD
-			JOIN inserted I ON I.hoaDon = HD.maHoaDon
-		END
-		
-		IF EXISTS (
-			SELECT 1
-			FROM inserted I
-			JOIN GiaoDich GD ON I.hoaDon = GD.hoaDon
-			WHERE I.tinhTrang = N'Thất bại'
-			GROUP BY I.hoaDon
-			HAVING COUNT(GD.hoaDon) >= 3
-		)
-		BEGIN
-			PRINT N'Bạn không thể giao dịch nữa. Đơn hàng bị hủy.';
-			UPDATE HD
-			SET HD.tinhTrang = N'Đã Hủy'
-			FROM HoaDon HD
-			JOIN inserted I ON I.hoaDon = HD.maHoaDon;
-
-			DELETE GD
-			FROM GiaoDich GD
-			JOIN inserted I ON GD.hoaDon = I.hoaDon;
-		END
-		END
-	END;
 
 GO
 CREATE OR ALTER TRIGGER trg_HashNguoiDungPassword
--- sql/triggers.sql
+++ sql/triggers.sql
@@ -162,3 +162,62 @@ BEGIN
 		ON DH.hoaDon = HD.maHoaDon;
 	END
 END;
+
+
+
+GO
+CREATE OR ALTER TRIGGER trg_CheckGiaoDich
+ON GiaoDich
+AFTER INSERT, UPDATE
+AS
+BEGIN
+    IF TRIGGER_NESTLEVEL() > 1
+        RETURN;
+
+    IF EXISTS (SELECT 1 FROM inserted)
+	BEGIN
+		IF EXISTS(
+			SELECT 1
+			FROM HoaDon H
+			JOIN inserted I ON H.maHoaDon = I.hoaDon
+			WHERE H.tinhTrang = N'Đã thanh toán' or H.tinhTrang = N'Đã huỷ'
+		)
+		BEGIN
+			ROLLBACK TRANSACTION;
+			THROW 50002, N'Bạn không thể thực hiện giao dịch trên đơn hàng đã thanh toán hoặc đã hủy', 1;
+		END
+		
+		IF EXISTS(
+			SELECT 1
+			FROM inserted I
+			WHERE I.tinhTrang = N'Thành công'
+		)
+		BEGIN
+			PRINT N'Bạn đã thanh toán thành công';
+			UPDATE HD
+			SET HD.tinhTrang = N'Đã Thanh Toán'
+			FROM HoaDon HD
+			JOIN inserted I ON I.hoaDon = HD.maHoaDon
+		END
+		
+		IF EXISTS (
+			SELECT 1
+			FROM inserted I
+			JOIN GiaoDich GD ON I.hoaDon = GD.hoaDon
+			WHERE I.tinhTrang = N'Thất bại'
+			GROUP BY I.hoaDon
+			HAVING COUNT(GD.hoaDon) >= 3
+		)
+		BEGIN
+			UPDATE HD
+			SET HD.tinhTrang = N'Đã huỷ'
+			FROM HoaDon HD
+			JOIN inserted I ON I.hoaDon = HD.maHoaDon;
+
+			DELETE GD
+			FROM GiaoDich GD
+			JOIN inserted I ON GD.hoaDon = I.hoaDon;
+			THROW 50002, N'Bạn không thể giao dịch nữa. Đơn hàng bị hủy.', 1;
+		END
+    END
+END;
